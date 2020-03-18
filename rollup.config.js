5e87b6524479fe8ceadd8564ea617fee49ac8664
tonylefler <tonylefler@gmail.com>
2020-03-18 15:17:38 -0600

    dont need plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   output: {
     format: 'cjs'
   },
   external: ['electron', 'rxjs'],
-  plugins: [resolve(), commonjs()],
 };
