4130edecc6cb6bfc40d3cdca5e0726f377d0a473
Tony Lefler <tonylefler@gmail.com>
2020-04-09 15:47:11 -0600

    Remove rxjs and use promises (#6)
    * use promises
    
    * remove rxjs dep

--- src/brillo-renderer.js
+++ src/brillo-renderer.js
@@ -1,5 +1,4 @@
 import { ipcRenderer } from 'electron';
-import { Subject } from 'rxjs';
 
 import { isObject } from './shared';
 
@@ -14,21 +13,27 @@ class BrilloRenderer {
   constructor() {
     this.__actions = {};
     this.__listenerMap = new Map();
+    this.__eventMap = new Map();
     this.__index = 0;
 
     if (process && process.type === 'renderer') {
       ipcRenderer.on(mainToRenderer, (e, id, action, payload) => {
         if (this.__actions[action]) {
           this.__actions[action](payload).then((res) => {
-            ipcRenderer.send(rendererToMainResponse, id, res);
+            ipcRenderer.send(rendererToMainResponse, id, 'resolve', res);
+          }, (err) => {
+            ipcRenderer.send(rendererToMainResponse, id, 'reject', err);
+          }).catch(err => {
+            const callback = this.__eventMap.get('error');
+            if (callback) callback(err);
           });
         }
       });
 
-      ipcRenderer.on(mainToRendererResponse, (e, id, payload) => {
-        const { obs, action } = this.__listenerMap.get(id);
-        if (obs) {
-          obs.next(payload);
+      ipcRenderer.on(mainToRendererResponse, (e, id, promiseAction, payload) => {
+        const { promise, action } = this.__listenerMap.get(id);
+        if (promise) {
+          promise[promiseAction](payload);
           this.__listenerMap.delete(id);
         }
       });
@@ -48,11 +53,18 @@ class BrilloRenderer {
 
   talk(action, payload) {
     const id = this.__index;
-    const obs = new Subject();
-    this.__listenerMap.set(id, { obs, action });
+    let resolve, reject;
+    const promise = new Promise((_resolve, _reject) => {
+      resolve = _resolve;
+      reject = _reject;
+    });
+    this.__listenerMap.set(id, {
+      promise: { resolve, reject },
+      action,
+    });
     ipcRenderer.send(rendererToMain, id, action, payload);
     this.__index++
-    return obs;
+    return promise;
   }
 }
 
